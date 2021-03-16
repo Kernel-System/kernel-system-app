cdcb76c45c19eb9b0dde3e520439825733f939d6
DaNerOl <davidneriolguin@gmail.com>
2021-03-16 13:53:09 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Button type="primary">ANT DESIGN BUTTON</Button>
+    </div>
+  );
+}
+
+export default App;
