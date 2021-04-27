0ea33acd4f61b28486d0cb04f1ca2bf014058733
DaNerOl <davidneriolguin@gmail.com>
2021-04-27 16:02:21 -0600

    style: organización de carpetas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import './App.css';
-import { Button } from 'antd';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Button type="primary">ANT DESIGN BUTTON</Button>
-    </div>
-  );
+  return <div className="App">Hola mundo</div>;
 }
 
 export default App;
