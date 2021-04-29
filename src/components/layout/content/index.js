0a92f14a1e89f9f470ea77c54cf5ac39ba3397cb
DaNerOl <davidneriolguin@gmail.com>
2021-04-29 12:02:55 -0600

    feat(facturas): add LectorFacturas component

--- src/components/layout/content/index.js
+++ src/components/layout/content/index.js
@@ -1,19 +1,26 @@
-import "./style.css";
-import { Layout } from "antd";
+import './style.css';
+import { Layout } from 'antd';
+import LectorProveedores from 'components/util/facturas/LectorFacturas';
+
 const { Content } = Layout;
+
+const imprimirProveedores = (factura) => {
+  console.log(factura['cfdi:Emisor'][0].$);
+};
 
 const Index = () => {
   return (
-    <Layout style={{ padding: "10px 10px 0" }}>
+    <Layout style={{ padding: '10px 10px 0' }}>
       <Content
         className="site-layout-background"
         style={{
           padding: 24,
           margin: 0,
-          minHeight: "calc(100vh - 70px)",
+          minHeight: 'calc(100vh - 70px)',
         }}
       >
         Soy contenido xd
+        <LectorProveedores onSuccess={imprimirProveedores} />
       </Content>
     </Layout>
   );
---
+++ src/components/util/facturas/LectorFacturas/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Upload, message } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+
+const { Dragger } = Upload;
+
+const handleUpload = ({ file, onSuccess }) => {
+  setTimeout(() => {
+    onSuccess('ok');
+  }, 0);
+};
+
+var reader = new FileReader();
+
+const props = {
+  name: 'file',
+  multiple: false,
+  accept: '.xml',
+  maxCount: 1,
+  customRequest: handleUpload,
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      // console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      reader.readAsText(info.fileList[0].originFileObj);
+
+      message.success(
+        `El archivo ${info.file.name} ha sido subido exitosamente.`
+      );
+    } else if (status === 'error') {
+      message.error(`Fallo al subir el archivo ${info.file.name}.`);
+    }
+  },
+};
+
+const Index = ({ onSuccess }) => {
+  reader.onload = function () {
+    const result = reader.result;
+    convertXMLtoObject(result); //base64encoded string
+  };
+
+  function convertXMLtoObject(xml) {
+    var parseString = require('xml2js').parseString;
+    parseString(xml, function (err, result) {
+      onSuccess(result['cfdi:Comprobante']);
+    });
+  }
+
+  return (
+    <div>
+      <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">
+          Haga clic o arrastre un archivo a esta area para subirlo
+        </p>
+        <p className="ant-upload-hint">
+          Suba una factura en formato .xml para leer los datos del comprobante.
+        </p>
+      </Dragger>
+    </div>
+  );
+};
+
+export default Index;
