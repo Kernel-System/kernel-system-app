cf351bbcc5504e12c7fc991e492799dae957d1e6
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-05-04 12:53:14 -0600

    feat(TransferenciasList): crear componente de TransferenciasList

---
+++ src/components/list/TransferenciasList/index.js
@@ -0,0 +1,61 @@
+import './styles.css';
+import { List, Typography, Button, Badge, Select } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+const { Title, Text } = Typography;
+const { Option } = Select;
+
+const Index = ({ list }) => {
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+  return (
+    <>
+      <Title>Transferencias</Title>
+      <Text>Filtrar por:</Text>
+      <br />
+      <Select
+        defaultValue='Creado'
+        style={{ width: 120 }}
+        onChange={handleChange}
+      >
+        <Option value='1'>Pendiente</Option>
+        <Option value='2'>Tranferido</Option>
+        <Option value='3'>Recibido</Option>
+      </Select>
+      <List
+        itemLayout='vertical'
+        size='default'
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 10,
+        }}
+        dataSource={list}
+        renderItem={(item) => (
+          <Badge.Ribbon text={item.estado}>
+            <Link to={`/ensambles/${item.id}`}>
+              <List.Item key={item.id}>
+                <List.Item.Meta
+                  //avatar={<Avatar src={item.avatar} />}
+                  title={`Transferencia No. ${item.id}`}
+                  description={item.fechasolicitud}
+                />
+                {`De ${item.almacen_origen} a ${item.almacen_destino}`}
+              </List.Item>
+            </Link>
+          </Badge.Ribbon>
+        )}
+      />
+      <br />
+      <Link to='/ensambles/nuevo'>
+        <Button type='primary' size='large' icon={<PlusOutlined />}>
+          Añadir Nueva Transferencia
+        </Button>
+      </Link>
+    </>
+  );
+};
+
+export default Index;
