4b72832fa81eb7ab3cf6cf4568afe2f97ea250d7
Salvador G <nightlyflux@gmail.com>
2021-05-06 11:04:26 -0600

    feat(shared): add BoughtProductsList

---
+++ src/components/shared/BoughtProductsList/index.js
@@ -0,0 +1,29 @@
+import { Avatar, List, Typography } from 'antd';
+import { formatPrice } from 'utils';
+const { Paragraph } = Typography;
+
+const BoughtProductsList = ({ data }) => {
+  return (
+    <List
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={
+              <Avatar
+                size='large'
+                shape='square'
+                src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
+              />
+            }
+            title={<a href='https://ant.design'>{item}</a>}
+            description={formatPrice(420)}
+          />
+          <Paragraph>x1</Paragraph>
+        </List.Item>
+      )}
+    />
+  );
+};
+
+export default BoughtProductsList;
