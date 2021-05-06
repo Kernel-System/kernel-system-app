10b611122a5656ce7bbc39fc5cf8f9fe9b10230c
Salvador G <nightlyflux@gmail.com>
2021-05-06 12:15:48 -0600

    refactor(layout): complete Footer design

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -1,75 +1,104 @@
-import './style.css';
-import { Layout, Typography, Card, Image } from 'antd';
+import {
+  Layout,
+  Typography,
+  Image,
+  Space,
+  Row,
+  Button,
+  Col,
+  Avatar,
+} from 'antd';
+import { blue } from '@ant-design/colors';
+import { ShopOutlined, PhoneOutlined, MailOutlined } from '@ant-design/icons';
+import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
 const { Footer } = Layout;
-const { Link, Text } = Typography;
-
-const gridStyle = {
-  width: '50%',
-  textAlign: 'center',
-};
-
-const gridStyle2 = {
-  width: '100%',
-  textAlign: 'left',
-};
+const { Text } = Typography;
 
 const Index = () => {
+  const breakpoint = useBreakpoint();
+
   return (
     <Footer>
-      <div style={{ float: 'left', width: 300, position: 'flex' }}>
-        <Card.Grid size='small' hoverable={false} style={gridStyle}>
-          <Link>Inicio</Link>
-        </Card.Grid>
-        <Card.Grid size='small' hoverable={false} style={gridStyle}>
-          <Link>Facturación</Link>
-        </Card.Grid>
-        <Card.Grid size='small' hoverable={false} style={gridStyle}>
-          <Link>Categorias</Link>
-        </Card.Grid>
-        <Card.Grid size='small' hoverable={false} style={gridStyle}>
-          <Link>Aviso Legal</Link>
-        </Card.Grid>
-        <Card.Grid size='small' hoverable={false} style={gridStyle}>
-          <Link>Perfil</Link>
-        </Card.Grid>
-        <Card.Grid size='small' hoverable={false} style={gridStyle}>
-          <Link>Términos y condiciones</Link>
-        </Card.Grid>
-      </div>
-      <div
-        style={{
-          float: 'left',
-          position: 'flex',
-          marginLeft: '25%',
-        }}
-      >
-        <Image
-          width={200}
-          preview={false}
-          src='https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
-        />
-        <br />
-        <Text style={{ textAlign: 'center' }}>Kernel System © 2021</Text>
-      </div>
-      <div style={{ float: 'right', width: 300, position: 'flex' }}>
-        <Card.Grid size='small' hoverable={false} style={gridStyle2}>
-          <Link>Inicio</Link>
-        </Card.Grid>
-        <Card.Grid size='small' hoverable={false} style={gridStyle2}>
-          <Link>Facturación</Link>
-        </Card.Grid>
-        <Card.Grid size='small' hoverable={false} style={gridStyle2}>
-          <Link>Categorias</Link>
-        </Card.Grid>
-      </div>
+      <Row align='middle' justify='space-between' gutter={[24, 24]}>
+        <Col xs={24} lg={7}>
+          <Row gutter={[0, 8]}>
+            <Col xs={24} sm={12}>
+              <Button type='text' size='small' block={breakpoint.xs}>
+                Inicio
+              </Button>
+            </Col>
+            <Col xs={24} sm={12}>
+              <Button type='text' size='small' block={breakpoint.xs}>
+                Facturación
+              </Button>
+            </Col>
+            <Col xs={24} sm={12}>
+              <Button type='text' size='small' block={breakpoint.xs}>
+                Categorias
+              </Button>
+            </Col>
+            <Col xs={24} sm={12}>
+              <Button type='text' size='small' block={breakpoint.xs}>
+                Aviso Legal
+              </Button>
+            </Col>
+            <Col xs={24} sm={12}>
+              <Button type='text' size='small' block={breakpoint.xs}>
+                Perfil
+              </Button>
+            </Col>
+            <Col xs={24} sm={12}>
+              <Button type='text' size='small' block={breakpoint.xs}>
+                Términos y condiciones
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={24} lg={10}>
+          <Space
+            direction='vertical'
+            style={{ textAlign: 'center', width: '100%' }}
+          >
+            <Image
+              width={80}
+              height={80}
+              preview={false}
+              src='https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+            />
+            <Text>Kernel System © 2021</Text>
+          </Space>
+        </Col>
+        <Col xs={24} lg={7}>
+          <Space direction='vertical' size='middle'>
+            <Space>
+              <Avatar
+                icon={<ShopOutlined />}
+                style={{ backgroundColor: blue.primary }}
+              />
+              <Text>
+                Calle Márquez de León y Lic. Verdad Col. Centro, C.P. 23000 La
+                Paz, Baja California Sur
+              </Text>
+            </Space>
+            <Space>
+              <Avatar
+                icon={<PhoneOutlined />}
+                style={{ backgroundColor: blue.primary }}
+              />
+              <Text>(612) 348-92-16</Text>
+            </Space>
+            <Space>
+              <Avatar
+                icon={<MailOutlined />}
+                style={{ backgroundColor: blue.primary }}
+              />
+              <Text>kernelsystem@gmail.com</Text>
+            </Space>
+          </Space>
+        </Col>
+      </Row>
     </Footer>
   );
 };
-
-/*
-        <Link href="https://ant.design" target="_blank">
-          Ant Design (Link)
-        </Link>
-*/
 
 export default Index;
