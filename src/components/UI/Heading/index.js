514aff3ab490e046515df5219eaefe87242d2768
Salvador G <nightlyflux@gmail.com>
2021-05-06 13:46:53 -0600

    refactor(ui): add style property

--- src/components/UI/Heading/index.js
+++ src/components/UI/Heading/index.js
@@ -1,10 +1,12 @@
 import { Space, Typography } from 'antd';
 const { Title, Paragraph, Text } = Typography;
 
-const Heading = ({ title, subtitle, extra }) => (
+const Heading = ({ title, subtitle, extra, style }) => (
   <>
     <Space style={{ width: '100%' }}>
-      <Title level={3}>{title}</Title>
+      <Title level={3} style={style}>
+        {title}
+      </Title>
       {extra && <Text type='secondary'>{extra}</Text>}
     </Space>
     {subtitle && <Paragraph>{subtitle}</Paragraph>}
