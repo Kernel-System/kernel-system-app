052da69ae3e6bdb07f48356a4b4b8b0c0862516a
Salvador G <nightlyflux@gmail.com>
2021-05-06 13:47:35 -0600

    refactor(rutas): update routes

--- src/Rutas.js
+++ src/Rutas.js
@@ -1,11 +1,19 @@
-import { Route, Switch } from 'react-router';
+import ProveedorList from 'components/list/ProveedorList';
 import asyncComponent from 'hoc/asyncComponent';
+import MovimientosAlmacen from 'pages/almacen/MovimientosAlmacen';
+import NuevoMovimiento from 'pages/almacen/NuevoMovimiento';
 import Login from 'pages/auth/Login';
 import Cart from 'pages/Cart';
 import Checkout from 'pages/Checkout';
+import RegistrarCompra from 'pages/compras/Compras/RegistrarCompra';
 import AñadirProveedor from 'pages/compras/Proveedores/AñadirProveedor';
+import AgregarEnsambles from 'pages/ensamble/AgregarEnsambles';
+import Ensambles from 'pages/ensamble/Ensambles';
+import ModificarEnsambles from 'pages/ensamble/ModificarEnsamble';
+import FacturarTicket from 'pages/FacturarTicket';
 import Home from 'pages/Home';
 import NotFound from 'pages/NotFound';
+import Order from 'pages/orders/Order';
 import Orders from 'pages/orders/Orders';
 import Product from 'pages/Product';
 import Addresses from 'pages/profile/Addresses';
@@ -13,15 +21,9 @@ import ChangePassword from 'pages/profile/ChangePassword';
 import NewAddress from 'pages/profile/NewAddress';
 import Profile from 'pages/profile/Profile';
 import Search from 'pages/Search';
-import RegistrarCompra from 'pages/compras/Compras/RegistrarCompra';
-import ProveedorList from './components/list/ProveedorList';
-import AgregarEnsambles from './pages/ensamble/AgregarEnsambles';
-import Ensambles from './pages/ensamble/Ensambles';
-import ModificarEnsambles from './pages/ensamble/ModificarEnsamble';
-import FacturarTicket from './pages/FacturarTicket';
-import MovimientosAlmacen from './pages/almacen/MovimientosAlmacen';
-import NuevoMovimiento from './pages/almacen/NuevoMovimiento';
-import Order from 'pages/orders/Order';
+import PuntoDeVenta from 'pages/ventas/PuntoDeVenta';
+import Venta from 'pages/ventas/Venta';
+import { Route, Switch } from 'react-router';
 
 const asyncForgotPassword = asyncComponent(() =>
   import('pages/auth/ForgotPassword')
@@ -64,9 +66,9 @@ const Rutas = () => {
       <Route path='/direcciones/nueva' exact component={NewAddress} />
 
       {/* Profile */}
-      <Route path='/u/:id' exact component={Profile} />
+      <Route path='/perfil' exact component={Profile} />
       <Route
-        path='/u/:id/cambiar-contrasena'
+        path='/perfil/cambiar-contrasena'
         exact
         component={ChangePassword}
       />
@@ -87,8 +89,8 @@ const Rutas = () => {
       <Route path='/pedidos/:id' exact component={Order} />
 
       {/* Punto de venta */}
-      {/* <Route path='/venta' exact component={PuntoDeVenta} /> */}
-      {/* <Route path='/venta/nueva' exact component={PuntoDeVenta} /> */}
+      <Route path='/venta' exact component={PuntoDeVenta} />
+      <Route path='/venta/nueva' exact component={Venta} />
 
       {/* Proveedores */}
       <Route path='/registrar-compra' exact component={RegistrarCompra} />
