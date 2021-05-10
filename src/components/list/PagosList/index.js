0f111eeb03f84aabd333c57dc5ea50a5a867150d
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-05-10 10:16:58 -0600

    feat(PagosModal): crear componente PagosModal

--- src/components/list/PagosList/index.js
+++ src/components/list/PagosList/index.js
@@ -1,13 +1,15 @@
 import './styles.css';
-import { List, Typography, Button, Badge } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
-const { Title } = Typography;
+import { List, Badge } from 'antd';
+import { useState } from 'react';
+import PagosModal from 'components/pagos/PagosModal';
+import HeaderBack from 'components/UI/HeadingBack';
 
 const Index = ({ list }) => {
+  const [visible, setVisible] = useState(false);
+  const [clave, setClave] = useState('');
   return (
     <>
-      <Title>Compras</Title>
+      <HeaderBack title='Pagos' />
       <List
         itemLayout='vertical'
         size='default'
@@ -19,26 +21,32 @@ const Index = ({ list }) => {
         }}
         dataSource={list}
         renderItem={(item) => (
-          <Badge.Ribbon text={item.fecha_entrega}>
-            <Link to={`/ensambles/${item.idPago}`}>
-              <List.Item key={item.idPago}>
-                <List.Item.Meta
-                  //avatar={<Avatar src={item.avatar} />}
-                  title={`Compra No. ${item.idPago}`}
-                  description={item.FechaPago}
-                />
-                {`${item.factura} $`}
-              </List.Item>
-            </Link>
+          <Badge.Ribbon text={item.fecha_pago}>
+            <List.Item
+              key={item.id_pago}
+              onClick={() => {
+                setVisible(true);
+                setClave(item.id_pago);
+              }}
+            >
+              <List.Item.Meta
+                //avatar={<Avatar src={item.avatar} />}
+                title={`Id de Pago: ${item.id_pago}`}
+                description={`Pago No: ${item.num_operacion}`}
+              />
+              {`Identificador de Factura: ${item.factura}`}
+            </List.Item>
           </Badge.Ribbon>
         )}
       />
-      <br />
-      <Link to='/ensambles/nuevo'>
-        <Button type='primary' size='large' icon={<PlusOutlined />}>
-          Añadir Compra
-        </Button>
-      </Link>
+      <PagosModal
+        visible={visible}
+        clave={clave}
+        setVis={() => {
+          setVisible(false);
+          setClave('');
+        }}
+      />
     </>
   );
 };
