6e44e4252870cb4b83eade07bdb9f6797fc1d2ce
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-05-18 13:53:22 -0600

    refactor(api): agregar bearer y cambio baseURL local

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const http = axios.create({
-  baseURL: 'https://kernel-system-api.herokuapp.com/',
+  baseURL: 'http://localhost:8055',
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijk2NGIyNjRiLTdmMzktNDZlOC1hZmEwLWVjM2ZjYmQ1MWY3YSIsImlhdCI6MTYyMTAyMTUzMSwiZXhwIjoxNjIxMDI1MTMxfQ.qAt8HPaWGNJhXhgfEg-glWQgZYY7nIfdk1O2uL8i0T0`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijk2NGIyNjRiLTdmMzktNDZlOC1hZmEwLWVjM2ZjYmQ1MWY3YSIsImlhdCI6MTYyMTM2NTQ5NywiZXhwIjoxNjIxMzY5MDk3fQ.WUi0jT3IJf1zQKuQf701qiZ74QCMDf-fHx6-3d8wcqQ`,
   },
 });
