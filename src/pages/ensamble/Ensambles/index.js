888ca6211c042c8f752ce16a9023c31ca7f3ccab
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-05-18 13:52:35 -0600

    refactor(Ensamble): agregar busqueda por estado sin finalizar

--- src/pages/ensamble/Ensambles/index.js
+++ src/pages/ensamble/Ensambles/index.js
@@ -1,35 +1,58 @@
 import EnsambleList from 'components/list/EnsambleList';
+import { Typography, Select } from 'antd';
 import { useEffect, useState } from 'react';
 import { http } from 'api';
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const Index = () => {
-  const [pag, usePag] = useState(1);
-  const [list, useList] = useState([]);
+  const [pag, setPag] = useState(1);
+  const [list, setList] = useState([]);
+  const [estado, setEstado] = useState('Ordenado');
 
   useEffect(() => {
     http
       .get(
         `/items/ordenes_ensamble?offset=${5 * (pag - 1)}&?limit=${
           5 * pag
-        }?fields=folio,fechaorden,estado`
+        }&?fields=folio,fechaorden,estado&?filter={"estado":${'Ingresado en almacén'}}`
       )
       .then((result) => {
         //console.log(result.data)
         ChangeList(result.data.data);
       });
-  }, [pag]);
+  }, [estado]);
+
+  function handleChange(value) {
+    setEstado(value);
+  }
 
   const ChangeList = (lista) => {
-    useList(lista);
+    setList(lista);
   };
 
   const ChangePag = (pagina) => {
-    usePag(pag);
+    setPag(pag);
   };
+
+  const ensamble = <EnsambleList list={list} changePag={ChangePag} />;
 
   return (
     <div>
-      <EnsambleList list={list} changePag={ChangePag} />
+      <Title level={3}>Órdenes de Ensamble</Title>
+      <Text>Ordenar por Estado:</Text>
+      <br />
+      <Select
+        defaultValue='Ordenado'
+        style={{ width: 120 }}
+        onChange={handleChange}
+      >
+        <Option value='Ordenado'>Ordenado</Option>
+        <Option value='En ensamble'>En ensamble</Option>
+        <Option value='Ensamblado'>Ensamblado</Option>
+        <Option value='Ingresado en almacén'>Ingresado en almacén</Option>
+      </Select>
+      {ensamble}
     </div>
   );
 };
