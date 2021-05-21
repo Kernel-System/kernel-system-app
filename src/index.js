4a98c1cafff2bd86b735a51fcb2f04cec7f54c3b
Salvador G <nightlyflux@gmail.com>
2021-05-21 15:09:14 -0600

    feat(app): add easy-peasy store and user model

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
+import { StoreProvider } from 'easy-peasy';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from 'react-router-scroll-top';
+import { store } from 'store';
 import App from './App';
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools initialIsOpen={false} />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </QueryClientProvider>,
+  <StoreProvider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StoreProvider>,
   document.getElementById('root')
 );
