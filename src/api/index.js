e393befdcd5a7e2c0c231fa0ca1d708c599d5a3f
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-05-24 13:09:53 -0600

    refactor(caatalogos): Agregar nuevos catalogos de moneda y categorias de productos

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const http = axios.create({
   baseURL: 'http://localhost:8055',
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijk2NGIyNjRiLTdmMzktNDZlOC1hZmEwLWVjM2ZjYmQ1MWY3YSIsImlhdCI6MTYyMTM2NTQ5NywiZXhwIjoxNjIxMzY5MDk3fQ.WUi0jT3IJf1zQKuQf701qiZ74QCMDf-fHx6-3d8wcqQ`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijk2NGIyNjRiLTdmMzktNDZlOC1hZmEwLWVjM2ZjYmQ1MWY3YSIsImlhdCI6MTYyMTg3MDk4NCwiZXhwIjoxNjIxOTA2OTg0fQ.THpoJH48D0R83NFVDE6W-aI9_hDNl28IpFM4msfv-oY`,
   },
 });
