33a2b586ea5e21ed0a87c562d9109beab138d98a
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-05-26 12:52:58 -0600

    refactor(Transferencias): Lista de transferencias, edicion, muestra y creacion

--- src/pages/almacen/Transferencias/index.js
+++ src/pages/almacen/Transferencias/index.js
@@ -1,20 +1,45 @@
+import { http } from 'api';
+import { Button, message, Space } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import { useMutation, useQueryClient } from 'react-query';
+import Header from 'components/UI/Heading';
+import CsvReader from 'components/shared/CsvReader';
 import TransferenciasList from 'components/list/TransferenciasList';
 
-const index = () => {
-  const list = [
-    {
-      id: '00001',
-      estado: 'Pendiente',
-      fechasolicitud: '01/01/21',
-      almacen_origen: '1',
-      almacen_destino: '2',
+const Index = () => {
+  const insertItems = (items) => {
+    return http.post('/items/productos', items);
+  };
+
+  const importarProductos = (datos) => {
+    const hide = message.loading('Importando productos..', 0);
+    insertMutation.mutate(datos, { onSuccess: hide });
+  };
+
+  const queryClient = useQueryClient();
+
+  const insertMutation = useMutation((formData) => insertItems(formData), {
+    onSuccess: () => {
+      message.success('Proveedores importados exitosamente');
+      queryClient.invalidateQueries('proveedores');
     },
-  ];
+  });
+
   return (
-    <div>
-      <TransferenciasList list={list} />
-    </div>
+    <>
+      <Header title='Transferencias' />
+      <TransferenciasList />
+      <br />
+      <Space direction='horizontal' align='baseline' style={{ width: '100%' }}>
+        <Link to='/transferencia/nuevo'>
+          <Button type='primary' icon={<PlusOutlined />}>
+            Añadir Nueva Transferencia
+          </Button>
+        </Link>
+      </Space>
+    </>
   );
 };
 
-export default index;
+export default Index;
