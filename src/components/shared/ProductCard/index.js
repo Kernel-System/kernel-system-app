70150ef96846698ee4b7b602bf79a3562014845c
Salvador Gómez <nightlyflux@gmail.com>
2021-06-03 14:27:02 -0600

    refactor(product): add Product functionality

--- src/components/shared/ProductCard/index.js
+++ src/components/shared/ProductCard/index.js
@@ -1,11 +1,11 @@
 import { PercentageOutlined } from '@ant-design/icons';
 import { Badge, Card, Image, Space, Typography } from 'antd';
 import { useHistory } from 'react-router-dom';
-import { formatPrice } from 'utils/functions';
+import { formatPrice, toPercent } from 'utils/functions';
 
 const { Title, Paragraph, Text } = Typography;
 
-const ProductCard = ({ product, descuento = 0.5 }) => {
+const ProductCard = ({ product }) => {
   const history = useHistory();
 
   const goToProduct = (productId) => {
@@ -15,10 +15,11 @@ const ProductCard = ({ product, descuento = 0.5 }) => {
   return (
     <Badge.Ribbon
       color='magenta'
-      style={{ display: descuento ? 'block' : 'none' }}
+      style={{ display: product.descuento > 0 ? 'block' : 'none' }}
       text={
         <Text style={{ color: '#fff' }}>
-          <PercentageOutlined /> Oferta!
+          {product.descuento}
+          <PercentageOutlined /> Descuento!
         </Text>
       }
     >
@@ -30,24 +31,36 @@ const ProductCard = ({ product, descuento = 0.5 }) => {
             width='100%'
             height={200}
             style={{ objectFit: 'contain', padding: '24px 24px 0' }}
-            alt={product.title}
-            src={product.image}
+            src={
+              product.imagenes.length > 0
+                ? `${process.env.REACT_APP_DIRECTUS_API_URL}/assets/${product.imagenes[0].directus_files_id}`
+                : undefined
+            }
             fallback='https://st4.depositphotos.com/17828278/24401/v/600/depositphotos_244011872-stock-illustration-no-image-vector-symbol-missing.jpg'
           />
         }
-        onClick={() => goToProduct(product.id)}
+        onClick={() => goToProduct(product.codigo)}
       >
-        <Text type='secondary'>{product.category.toUpperCase()}</Text>
+        <Text type='secondary'>
+          {product.categorias.map(
+            (categoria, i) =>
+              `${categoria.categorias_id.nombre.toUpperCase()}${
+                i < product.categorias.length - 1 ? ' / ' : ''
+              }`
+          )}
+        </Text>
         <Paragraph ellipsis={{ rows: 2 }} style={{ minHeight: 44 }}>
-          {product.title}
+          {product.titulo}
         </Paragraph>
         <Space>
           <Title level={3} style={{ display: 'inline-block', marginBottom: 0 }}>
-            {formatPrice(product.price * descuento)}
+            {formatPrice(product.costo * toPercent(product.descuento))}
           </Title>
-          <Text type={'secondary'} delete>
-            {formatPrice(product.price)}
-          </Text>
+          {product.descuento > 0 && (
+            <Text type={'secondary'} delete>
+              {formatPrice(product.costo)}
+            </Text>
+          )}
         </Space>
       </Card>
     </Badge.Ribbon>
