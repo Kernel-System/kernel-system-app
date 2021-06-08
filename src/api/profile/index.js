5e509493c9542450a19ff8bae7894437b247e732
Salvador Gómez <nightlyflux@gmail.com>
2021-06-08 14:28:23 -0600

    fix(profile): fix profile error

--- src/api/profile/index.js
+++ src/api/profile/index.js
@@ -7,15 +7,13 @@ export const getUserData = async (token) => {
     '/users/me?fields=id,first_name,last_name,email,role.name,cliente.*,empleado.*',
     { headers: { Authorization: `Bearer ${token}` } }
   );
-  console.log(user);
   return {
     id: user.id,
     nombre: user.first_name,
     apellidos: user.last_name,
     correo: user.email,
-    rol: user.role.name,
-    cliente: user.cliente[0],
-    empleado: user.empleado[0],
+    cliente: user.cliente ? user.cliente[0] : [],
+    empleado: user.empleado ? user.empleado[0] : [],
   };
 };
 
