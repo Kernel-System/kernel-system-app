eb4592238794d20d8dd91f86fa7f2d7aa48831d6
David Neri <davidneriolguin@gmail.com>
2021-06-08 13:31:01 -0600

    refactor(LectorFacturas): permitir estilo Dragger y default

--- src/components/shared/facturas/LectorFacturas/index.js
+++ src/components/shared/facturas/LectorFacturas/index.js
@@ -1,69 +1,82 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Upload, message } from 'antd';
+import { Upload, message, Button } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
+import { UploadOutlined } from '@ant-design/icons';
 
 const { Dragger } = Upload;
 
 const handleUpload = ({ file, onSuccess }) => {
-  setTimeout(() => {
-    onSuccess('ok');
-  }, 0);
+    setTimeout(() => {
+        onSuccess('ok');
+    }, 0);
 };
 
 var reader = new FileReader();
 
-const props = {
-  name: 'file',
-  multiple: false,
-  accept: '.xml',
-  maxCount: 1,
-  customRequest: handleUpload,
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      // console.log(info.file, info.fileList);
+const Index = ({
+    isDragger,
+    onSuccess,
+    hideUploadList,
+    titulo = 'Haga clic o arrastre un archivo a esta area para subirlo',
+}) => {
+    const props = {
+        name: 'file',
+        multiple: false,
+        accept: '.xml',
+        maxCount: 1,
+        customRequest: handleUpload,
+        onChange(info) {
+            const { status } = info.file;
+            if (status !== 'uploading') {
+                // console.log(info.file, info.fileList);
+            }
+            if (status === 'done') {
+                reader.readAsText(info.fileList[0].originFileObj);
+
+                if (!hideUploadList)
+                    message.success(
+                        `El archivo ${info.file.name} ha sido subido exitosamente.`
+                    );
+            } else if (status === 'error') {
+                message.error(`Fallo al cargar el archivo ${info.file.name}.`);
+            }
+        },
+    };
+    reader.onload = function () {
+        const result = reader.result;
+        convertXMLtoObject(result); //base64encoded string
+    };
+
+    function convertXMLtoObject(xml) {
+        var parseString = require('xml2js').parseString;
+        parseString(xml, function (err, result) {
+            onSuccess(result['cfdi:Comprobante']);
+        });
     }
-    if (status === 'done') {
-      reader.readAsText(info.fileList[0].originFileObj);
 
-      message.success(
-        `El archivo ${info.file.name} ha sido subido exitosamente.`
-      );
-    } else if (status === 'error') {
-      message.error(`Fallo al subir el archivo ${info.file.name}.`);
-    }
-  },
-};
-
-const Index = ({ onSuccess }) => {
-  reader.onload = function () {
-    const result = reader.result;
-    convertXMLtoObject(result); //base64encoded string
-  };
-
-  function convertXMLtoObject(xml) {
-    var parseString = require('xml2js').parseString;
-    parseString(xml, function (err, result) {
-      onSuccess(result['cfdi:Comprobante']);
-    });
-  }
-
-  return (
-    <div>
-      <Dragger {...props}>
-        <p className='ant-upload-drag-icon'>
-          <InboxOutlined />
-        </p>
-        <p className='ant-upload-text'>
-          Haga clic o arrastre un archivo a esta area para subirlo
-        </p>
-        <p className='ant-upload-hint'>
-          Suba una factura en formato .xml para leer los datos del comprobante.
-        </p>
-      </Dragger>
-    </div>
-  );
+    return (
+        <>
+            {isDragger ? (
+                <div>
+                    <Dragger {...props} showUploadList={!hideUploadList}>
+                        <p className='ant-upload-drag-icon'>
+                            <InboxOutlined />
+                        </p>
+                        <p className='ant-upload-text'>{titulo}</p>
+                        <p className='ant-upload-hint'>
+                            Suba una factura en formato .XML para leer los datos
+                            del comprobante.
+                        </p>
+                    </Dragger>
+                </div>
+            ) : (
+                <Upload {...props} showUploadList={!hideUploadList}>
+                    <Button icon={<UploadOutlined />}>{titulo}</Button>
+                </Upload>
+            )}
+        </>
+    );
 };
 
 export default Index;
