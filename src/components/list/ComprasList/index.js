3af323f78101071e2299abfa2a288d349ae6d761
David Neri <davidneriolguin@gmail.com>
2021-06-08 13:38:27 -0600

    refactor(ComprasList): cambiar mensajes, fechas y busqueda

--- src/components/list/ComprasList/index.js
+++ src/components/list/ComprasList/index.js
@@ -1,102 +1,88 @@
-import { useState } from 'react';
 import './styles.css';
-import { List, Typography, Button, Badge, Select, Modal } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
-import { Row, Col } from 'antd';
-import CompraForm from 'components/forms/CompraForm';
+import { useState } from 'react';
 import Header from 'components/UI/Heading';
+import { Popconfirm, List, Typography, Button, Badge, Select } from 'antd';
+import { DeleteFilled, EditFilled } from '@ant-design/icons';
+import { Row, Col } from 'antd';
+import { useQuery } from 'react-query';
+import { getItems } from 'api/shared/compras';
+import moment from 'moment';
+import 'moment/locale/es-mx';
+import locale from 'antd/es/date-picker/locale/es_ES';
+
+const formatoCompra = 'DD MMMM YYYY, h:mm:ss a';
+const formatoEntrega = 'DD/MM/YYYY';
 
 const { Text } = Typography;
 const { Option } = Select;
 
-const Index = ({ list }) => {
-  list = [
-    {
-      no_compra: '1',
-      rfc_proveedor: 'HDM001017AS1',
-      nombre_proveedor: 'HOME DEPOT MEXICO S. DE R.L. DE C.V.',
-      folio: '',
-      fecha_compra: '2021-02-20T20:08:27',
-      fecha_entrega: '2021-03-23T20:08:27',
-      moneda: '',
-      tipo_cambio: '',
-      subtotal: '',
-      total: '',
-      forma_pago: '',
-      metodo_pago: '',
-    },
-    {
-      no_compra: '2',
-      rfc_proveedor: 'CFE000814QH8',
-      nombre_proveedor: 'EL CLAVO FERRETERIA S.A. DE C.V.',
-      folio: '',
-      fecha_compra: '2021-02-20T16:10:16',
-      fecha_entrega: '2021-02-21T16:10:16',
-      moneda: '',
-      tipo_cambio: '',
-      subtotal: '',
-      total: '',
-      forma_pago: '',
-      metodo_pago: '',
-    },
-    {
-      no_compra: '3',
-      rfc_proveedor: 'SSC840823JT3',
-      nombre_proveedor: 'SISTEMAS Y SERVICIOS DE COMUNICACION, S.A. DE C.V.',
-      folio: '',
-      fecha_compra: '2021-02-17T16:06:46',
-      fecha_entrega: '2021-02-18T16:06:46',
-      moneda: '',
-      tipo_cambio: '',
-      subtotal: '',
-      total: '',
-      forma_pago: '',
-      metodo_pago: '',
-    },
-  ];
+const Index = ({ editItem, onConfirmDelete, onClickItem }) => {
+  const [searchValue, setSearchValue] = useState('');
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [listElement, setListElement] = useState({});
-  const [listToShow, setListToShow] = useState(list);
-
-  const showModal = (listElement) => {
-    setIsModalVisible(true);
-    setListElement(listElement);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
+  function filtrarPorProveedor(compras, rfc) {
+    if (compras && rfc)
+      setListToShow(compras.filter((item) => item.rfc_emisor === rfc));
+    else {
+      setListToShow(compras);
+    }
+  }
 
   function onChange(value) {
-    if (value === undefined) setListToShow(list);
-    else
-      setListToShow(
-        list.filter((item) => item.rfc_proveedor.includes(value.toUpperCase()))
-      );
+    setSearchValue(value);
+    filtrarPorProveedor(list, value);
   }
 
   function onSearch(value) {
-    setListToShow(
-      list.filter((item) =>
-        item.nombre_proveedor.toLowerCase().includes(value.toLowerCase())
-      )
-    );
+    if (list && value)
+      setListToShow(
+        list.filter((item) => item.nombre_emisor.includes(value.toUpperCase()))
+      );
+    else setListToShow(list);
+  }
+
+  function ordenarPorRecientes(lista) {
+    return lista
+      .slice()
+      .sort((a, b) => new Date(b.fecha_compra) - new Date(a.fecha_compra));
+  }
+
+  function ordenarPorAntiguos(lista) {
+    return lista
+      .slice()
+      .sort((a, b) => new Date(a.fecha_compra) - new Date(b.fecha_compra));
   }
 
   function handleSort(value) {
     switch (value) {
-      case 'reciente':
-        setListToShow(list.slice().sort((a, b) => a.no_compra - b.no_compra));
-        break;
       case 'antigua':
-        setListToShow(list.slice().sort((a, b) => b.no_compra - a.no_compra));
+        setListToShow(ordenarPorAntiguos(list));
+        break;
+      case 'reciente':
+        setListToShow(ordenarPorRecientes(list));
         break;
       default:
         break;
     }
   }
+
+  const { data: list } = useQuery('compras', async () => {
+    const { data } = await getItems();
+    const datosOrdenados = ordenarPorRecientes(data.data);
+    const compras = [];
+    const newProveedores = [];
+    datosOrdenados.forEach(({ factura, ...elem }) => {
+      const { rfc_emisor, nombre_emisor } = factura;
+      compras.push({ ...factura, ...elem });
+      if (!newProveedores.some((prov) => prov.rfc_emisor === rfc_emisor))
+        newProveedores.push({ rfc_emisor, nombre_emisor });
+    });
+    setListToShow(compras);
+    setProveedores(newProveedores);
+    filtrarPorProveedor(compras, searchValue);
+    return compras;
+  });
+  const [proveedores, setProveedores] = useState([]);
+  const [listToShow, setListToShow] = useState([]);
 
   return (
     <>
@@ -105,6 +91,7 @@ const Index = ({ list }) => {
         <Col xs={24} lg={12}>
           <Select
             allowClear
+            value={searchValue}
             showSearch
             style={{ width: '100%' }}
             placeholder='Buscar por proveedor'
@@ -117,15 +104,11 @@ const Index = ({ list }) => {
                 .includes(input.toLowerCase());
             }}
           >
-            <Option value='HDM001017AS1'>
-              HOME DEPOT MEXICO S. DE R.L. DE C.V.
-            </Option>
-            <Option value='CFE000814QH8'>
-              EL CLAVO FERRETERIA S.A. DE C.V.
-            </Option>
-            <Option value='SSC840823JT3'>
-              SISTEMAS Y SERVICIOS DE COMUNICACION, S.A. DE C.V.
-            </Option>
+            {proveedores.map((proveedor, index) => (
+              <Option key={index} value={proveedor.rfc_emisor}>
+                {proveedor.nombre_emisor}
+              </Option>
+            ))}
           </Select>
         </Col>
         <Col>
@@ -137,21 +120,20 @@ const Index = ({ list }) => {
             Ordenar por:
           </Text>
         </Col>
-        <Col flex={1}>
+        <Col flex={1} style={{ paddingLeft: 0, paddingRight: 0 }}>
           <Select
             defaultValue='reciente'
             style={{ width: '100%' }}
             onChange={handleSort}
           >
             <Option value='reciente'>Más reciente</Option>
-            <Option value='antigua'>Más antiguas</Option>
+            <Option value='antigua'>Más antigua</Option>
           </Select>
         </Col>
       </Row>
       <br />
-
       <List
-        itemLayout='vertical'
+        itemLayout='horizontal'
         size='default'
         pagination={{
           onChange: (page) => {
@@ -160,48 +142,81 @@ const Index = ({ list }) => {
           pageSize: 10,
         }}
         dataSource={listToShow}
-        renderItem={(item) => (
-          <Badge.Ribbon text={item.fecha_entrega}>
-            <List.Item key={item.no_compra}>
+        renderItem={(item, index) => (
+          <Badge.Ribbon
+            locale={locale}
+            key={index}
+            style={{
+              top: -12,
+            }}
+            text={
+              item.fecha_entrega ? (
+                <>
+                  {'Entrega:'}{' '}
+                  <b>{moment(item.fecha_entrega).format(formatoEntrega)}</b>
+                </>
+              ) : (
+                'Sin fecha de entrega'
+              )
+            }
+          >
+            <List.Item
+              key={item.no_compra}
+              actions={[
+                <Button
+                  icon={<EditFilled />}
+                  onClick={() => editItem(item)}
+                ></Button>,
+                <Popconfirm
+                  placement='left'
+                  title='¿Está seguro de querer borrar este registro?'
+                  okText='Sí'
+                  cancelText='No'
+                  onConfirm={() => onConfirmDelete(item)}
+                >
+                  <Button danger icon={<DeleteFilled />}></Button>
+                </Popconfirm>,
+              ]}
+            >
               <List.Item.Meta
                 title={
                   <p
                     onClick={() => {
-                      showModal(item);
+                      onClickItem(item);
                     }}
                     style={{
                       cursor: 'pointer',
                       margin: 0,
                     }}
                   >
-                    {item.nombre_proveedor}
+                    {item.nombre_emisor}
                   </p>
                 }
-                description={`Comprado el ${item.fecha_compra}`}
+                description={
+                  <p
+                    style={{
+                      margin: 0,
+                    }}
+                  >
+                    Fecha de compra:{' '}
+                    {moment(new Date(item.fecha_compra)).format(formatoCompra)}
+                  </p>
+                }
+                key={index}
               />
-              {`RFC: ${item.rfc_proveedor}`}
+              {
+                <h3
+                  style={{
+                    display: 'inline',
+                  }}
+                >
+                  TOTAL: ${item.total}
+                </h3>
+              }
             </List.Item>
           </Badge.Ribbon>
         )}
       />
-      <br />
-      <Link to='/compras/registrar'>
-        <Button type='primary' size='large' icon={<PlusOutlined />}>
-          Añadir Compra
-        </Button>
-      </Link>
-      <Modal
-        title={listElement.nombre_proveedor}
-        visible={isModalVisible}
-        footer={null}
-        onCancel={handleCancel}
-      >
-        <CompraForm
-          // onSubmit={compraActualizada}
-          submitText='Guardar cambios'
-          datosCompra={listElement}
-        />
-      </Modal>
     </>
   );
 };
