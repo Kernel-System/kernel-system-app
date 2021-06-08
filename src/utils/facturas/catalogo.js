38bda982b44e33f0af4a7bcaa2c7db3631d9cf0f
David Neri <davidneriolguin@gmail.com>
2021-06-08 13:29:29 -0600

    feat(catalogo): añadir datos de catalogo del SAT
    regimenesFiscales, usosCfdi, tiposDeComprobante, tiposRelacion

--- src/utils/facturas/catalogo.js
+++ src/utils/facturas/catalogo.js
@@ -27,3 +27,78 @@ export const metodosDePago = {
   PUE: 'Pago en una sola exhibición',
   PPD: 'Pago en parcialidades o diferido',
 };
+
+export const regimenesFiscales = {
+  601: 'General de Ley Personas Morales',
+  603: 'Personas Morales con Fines no Lucrativos',
+  605: 'Sueldos y Salarios e Ingresos Asimilados a Salarios',
+  606: 'Arrendamiento',
+  608: 'Demás ingresos',
+  609: 'Consolidación',
+  610: 'Residentes en el Extranjero sin Establecimiento Permanente en México',
+  611: 'Ingresos por Dividendos (socios y accionistas)',
+  612: 'Personas Físicas con Actividades Empresariales y Profesionales',
+  614: 'Ingresos por intereses',
+  616: 'Sin obligaciones fiscales',
+  620:
+    'Sociedades Cooperativas de Producción que optan por diferir sus ingresos',
+  621: 'Incorporación Fiscal',
+  622: 'Actividades Agrícolas, Ganaderas, Silvícolas y Pesqueras',
+  623: 'Opcional para Grupos de Sociedades',
+  624: 'Coordinados',
+  628: 'Hidrocarburos',
+  607: 'Régimen de Enajenación o Adquisición de Bienes',
+  629:
+    'De los Regímenes Fiscales Preferentes y de las Empresas Multinacionales',
+  630: 'Enajenación de acciones en bolsa de valores',
+  615: 'Régimen de los ingresos por obtención de premios',
+  625:
+    'Régimen de las Actividades Empresariales con ingresos a través de Plataformas Tecnológicas',
+};
+
+export const usosCfdi = {
+  G01: 'Adquisición de mercancias',
+  G02: 'Devoluciones, descuentos o bonificaciones',
+  G03: 'Gastos en general',
+  I01: 'Construcciones',
+  I02: 'Mobilario y equipo de oficina por inversiones',
+  I03: 'Equipo de transporte',
+  I04: 'Equipo de computo y accesorios',
+  I05: 'Dados, troqueles, moldes, matrices y herramental',
+  I06: 'Comunicaciones telefónicas',
+  I07: 'Comunicaciones satelitales',
+  I08: 'Otra maquinaria y equipo',
+  D01: 'Honorarios médicos, dentales y gastos hospitalarios.',
+  D02: 'Gastos médicos por incapacidad o discapacidad',
+  D03: 'Gastos funerales.',
+  D04: 'Donativos.',
+  D05:
+    'Intereses reales efectivamente pagados por créditos hipotecarios (casa habitación).',
+  D06: 'Aportaciones voluntarias al SAR.',
+  D07: 'Primas por seguros de gastos médicos.',
+  D08: 'Gastos de transportación escolar obligatoria.',
+  D09:
+    'Depósitos en cuentas para el ahorro, primas que tengan como base planes de pensiones.',
+  D10: 'Pagos por servicios educativos (colegiaturas)',
+  P01: 'Por definir',
+};
+
+export const tiposDeComprobante = {
+  I: 'Ingreso',
+  E: 'Egreso',
+  T: 'Traslado',
+  N: 'Nómina',
+  P: 'Pago',
+};
+
+export const tiposRelacion = {
+  '01': 'Nota de crédito de los documentos relacionados',
+  '02': 'Nota de débito de los documentos relacionados',
+  '03': 'Devolución de mercancía sobre facturas o traslados previos',
+  '04': 'Sustitución de los CFDI previos',
+  '05': 'Traslados de mercancias facturados previamente',
+  '06': 'Factura generada por los traslados previos',
+  '07': 'CFDI por aplicación de anticipo',
+  '08': 'Factura generada por pagos en parcialidades',
+  '09': 'Factura generada por pagos diferidos',
+};
