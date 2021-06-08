af915a760e594321cd1e85afd9c5f1674195c5fa
David Neri <davidneriolguin@gmail.com>
2021-06-08 13:41:54 -0600

    feat(Compras): agregar pagina de Compras

---
+++ src/pages/compras/Compras/index.js
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import { Button, Modal, message } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import { useMutation, useQueryClient } from 'react-query';
+import CompraForm from 'components/forms/CompraForm';
+import ListaCompras from 'components/list/ComprasList';
+import * as CRUD from 'api/shared/compras';
+import moment from 'moment';
+import 'moment/locale/es-mx';
+
+const Index = (props) => {
+  const showModal = (element) => {
+    setIsModalVisible(true);
+    setListElement(element);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const onConfirmDelete = async (item) => {
+    deleteMutation.mutate(item);
+  };
+
+  const onSaveChanges = (values) => {
+    const newValues = {
+      ...values,
+      no_compra: listElement.no_compra,
+      fecha_compra: moment(values.fecha_compra).format('YYYY-MM-DDThh:mm:ss'),
+      fecha_entrega: moment(values.fecha_entrega).format('YYYY-MM-DD'),
+    };
+    updateMutation.mutate(newValues);
+  };
+
+  const queryClient = useQueryClient();
+
+  const updateMutation = useMutation(CRUD.updateItem, {
+    onSuccess: () => {
+      queryClient
+        .invalidateQueries('compras')
+        .then(message.success('Cambios guardados exitosamente'));
+    },
+  });
+  const deleteMutation = useMutation(CRUD.deleteItem, {
+    onSuccess: () => {
+      queryClient
+        .invalidateQueries('compras')
+        .then(message.success('Registro eliminado exitosamente'));
+    },
+  });
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [listElement, setListElement] = useState({});
+
+  return (
+    <>
+      <ListaCompras
+        onClickItem={showModal}
+        editItem={showModal}
+        onConfirmDelete={onConfirmDelete}
+      ></ListaCompras>
+      <br />
+
+      <Link to='/compras/registrar'>
+        <Button type='primary' icon={<PlusOutlined />}>
+          Registrar Compra
+        </Button>
+      </Link>
+      <Modal
+        title={listElement.nombre_proveedor}
+        visible={isModalVisible}
+        footer={null}
+        onCancel={handleCancel}
+        width='70%'
+      >
+        <CompraForm
+          datosCompra={listElement}
+          onSubmit={onSaveChanges}
+          submitText='Guardar cambios'
+        />
+      </Modal>
+    </>
+  );
+};
+
+export default Index;
