86faa13c10d632e41fe17b1c6df3ab85719e8466
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-06-10 12:42:04 -0600

    feat(BoughtProductsListWithSeries): crear componente para ver productos con series

---
+++ src/components/shared/BoughtProductsListWithSeries/index.js
@@ -0,0 +1,37 @@
+import { Avatar, List, Select } from 'antd';
+const { Option } = Select;
+
+const BoughtProductsList = ({ products }) => {
+  return (
+    <List
+      dataSource={products}
+      renderItem={(item) => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={
+              item?.imagenes?.length !== 0 ? (
+                <Avatar
+                  shape='square'
+                  src={`${process.env.REACT_APP_DIRECTUS_API_URL}/assets/${item.imagenes[0].directus_files_id}`}
+                />
+              ) : null
+            }
+            title={item.titulo}
+            description={`Cantidad: ${item.cantidad}`}
+          />
+          {item?.series_producto_movimiento?.length !== 0 ? (
+            <Select style={{ width: '40%' }} optionFilterProp='children'>
+              {item.series_producto_movimiento.map((serie) => (
+                <Option key={serie.id} value={serie.id}>
+                  {serie.serie}
+                </Option>
+              ))}
+            </Select>
+          ) : null}
+        </List.Item>
+      )}
+    />
+  );
+};
+
+export default BoughtProductsList;
