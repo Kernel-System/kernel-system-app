845011307b476e9a5e45dff52c526345939d0e20
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-06-18 12:26:01 -0600

    feat(api): agregar httpSAT para agregar facturas

--- src/api/index.js
+++ src/api/index.js
@@ -4,4 +4,12 @@ export const http = axios.create({
   baseURL: process.env.REACT_APP_DIRECTUS_API_URL,
 });
 
+export const httpSAT = axios.create({
+  baseURL: process.env.REACT_APP_FACTURA_API_URL,
+  headers: {
+    contentType: 'application/json',
+    Authorization: `Basic ${process.env.REACT_APP_FACTURA_TOKEN_URL}`,
+  },
+});
+
 export const url = process.env.REACT_APP_DIRECTUS_API_URL;
